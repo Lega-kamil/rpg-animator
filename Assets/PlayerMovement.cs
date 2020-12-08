83df506c6cd65df8646e1782d8153a670a6c061c
Lega-kamil <lega-kamil@wp.pl>
2020-12-08 19:05:42 +0100

    Beginning of animator project.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+
+    Animator animator;
+    
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+   
+    void Update()
+    {
+        animator.SetFloat("vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Jump();
+        }
+
+    }
+    public void Jump()
+    {
+        animator.SetTrigger("jump");
+    }
+}
